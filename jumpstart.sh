5a87666188485271571b6f8fdf4ceca42f8c4a12
William Glasse <william.glasse@ibm.com>
2022-02-25 13:52:04 +0000

    feat: prettier shell script

--- jumpstart.sh
+++ jumpstart.sh
@@ -1,9 +1,8 @@
 !/bin/bash
 
-while [ -z $repository_name ]
-do
-    echo Hello, what is the name of your repository?
-    read repository_name
+while [ -z $repository_name ]; do
+  echo Hello, what is the name of your repository?
+  read repository_name
 done
 
 gh repo create $repository_name --template "https://github.com/william-glasse/node-starter" --public --clone
