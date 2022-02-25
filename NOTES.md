67155e587843c87a4a543d4d57aecb37347b7199
William Glasse <william.glasse@ibm.com>
2022-02-25 12:37:58 +0000

    feat: install and sed

--- NOTES.md
+++ NOTES.md
@@ -9,5 +9,10 @@ TODO:
 Pomodoro 1/2
 ✅ Cleanup jumpstart repository
     Remove redundant 
-- write gh clone template repositry 
+✅ write gh clone template repositry 
 (gh repo create jumpstart --template "https://github.com/william-glasse/node-starter" --public -c)
+
+Pomodorro 3
+- Swap the old project with the new one
+    - ✅ package.json
+- install the packages
--- jumpstart.sh
+++ jumpstart.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 while [ -z $repository_name ]
 do
@@ -7,3 +7,11 @@ do
 done
 
 gh repo create $repository_name --template "https://github.com/william-glasse/node-starter" --public --clone
+
+cd $repository_name
+
+echo Replacing references to template repo with your new repository name... 🚧
+sed -i '' "s/node-starter/$repository_name/" package.json
+echo Succesffully replaced references
+
+npm i
